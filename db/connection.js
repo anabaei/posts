140e93183caf6d0b3c2478aecb845ccef6fbe41d
anabaei <nabaei17@gmail.com>
2021-07-09 19:16:25 -0700

    add database name

--- db/connection.js
+++ db/connection.js
@@ -14,10 +14,22 @@ if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
 	});
 } else {
 	// the application is executed on the local machine
-	connection= new Sequelize('postgres://localhost:5432/aaa', { dialect: 'postgres' });
+	// connection= new Sequelize('postgres://localhost:5432/aaa', { dialect: 'postgres' });
+	connection = new Sequelize('postgres://vqnubtbgyglagl:2f96394187c5c34c947016b97aa4737786d5317bc11a451cae6623111a65f894@ec2-34-233-114-40.compute-1.amazonaws.com:5432/db0qumikoduahu', { dialect: 'postgres' });
 }
 connection.authenticate().then(() => {
 	console.log('Connection to db established successfully');
 });
 
 export default connection;
+
+// const { Client } = require('pg');
+
+// const client = new Client({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
+
+// client.connect();
