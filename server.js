00ea874165458b9c56974d9e01dbd174892b8894
anabaei <nabaei17@gmail.com>
2021-07-11 10:27:13 -0700

    add to heroku

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ import posts from './routes/posts.js';
 ////////// Allow to parse bodies in json //////////
 ///////////////////////////////////////////////////
 const app = Express()
-app.use(bodyParser.json()); // help us to handle json in body
-app.use(bodyParser.urlencoded({ extended: false})); // help us to have req.body in callbacks and reads what is inside body
+// app.use(bodyParser.json()); // help us to handle json in body
+// app.use(bodyParser.urlencoded({ extended: false})); // help us to have req.body in callbacks and reads what is inside body
 
-app.use('/users', users);
-app.use('/posts', posts);
+// app.use('/users', users);
+// app.use('/posts', posts);
 
 app.get('/', (request, response) => { 
-    
+    console.log(' >>>> ',process.env);
     response.json(`Hello, World!`) 
 })
 const port = process.env.port || 3000;
