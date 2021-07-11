d5ede912f3af7f965b50074f2337a9700f01be09
anabaei <nabaei17@gmail.com>
2021-07-11 14:19:13 -0700

    add to heroku

--- db/connection.js
+++ db/connection.js
@@ -13,7 +13,7 @@ let connection = null;
 // 		logging: true //false
 // 	});
 // } 
-console.log(">>><<<>>",process.env.DATABASE_URI);
+console.log(">>><<<>>",process.env);
  if(process.env.DATABASE_URL){
 	connection =  new Sequelize(`${process.env.DATABASE_URI}?sslmode=require`, null, null, {
 		url: process.env.DATABASE_URI,
