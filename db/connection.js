37aee881f8f12bc68f57427f3818b205a0a94609
anabaei <nabaei17@gmail.com>
2021-07-11 10:13:07 -0700

    add to heroku

--- db/connection.js
+++ db/connection.js
@@ -3,16 +3,17 @@
 import Sequelize from 'sequelize';
 /// first is for heroku
 let connection = null;
-if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
-	// the application is executed on Heroku ... use the postgres database
-	connection = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
-		dialect: 'postgres',
-		protocol: 'postgres',
-		port: match[4],
-		host: match[3],
-		logging: true //false
-	});
-} 
+// if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
+// 	// the application is executed on Heroku ... use the postgres database
+// 	connection = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
+// 		dialect: 'postgres',
+// 		protocol: 'postgres',
+// 		port: match[4],
+// 		host: match[3],
+// 		logging: true //false
+// 	});
+// } 
+console.log(process.env.DATABASE_URL);
  if(process.env.DATABASE_URL){
 	connection = new Sequelize(process.env.DATABASE_URL, {
 		dialect: 'postgres',
