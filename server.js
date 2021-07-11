eff9f7e6663b5eadc89dbcc1bc749ce11f488047
anabaei <nabaei17@gmail.com>
2021-07-11 10:47:53 -0700

    add

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ const app = Express()
 app.get('/', (request, response) => { 
     console.log(' >>>> ',process.env);
     response.json(`Hello, World!`) 
-})
-const port = process.env.port || 3000;
+})           
+const port = process.env.PORT || 3000;
 app.listen(
 port, ()=>console.log('server is running')
 )
