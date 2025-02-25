6918686f4ee466834a2d896935c3188986aa930d
anabaei <nabaei17@gmail.com>
2025-02-24 22:50:20 -0800

    update auth middleware to disable JWT verification; add service field to Treatment model; enhance specialist and treatment routes; add clinic_id to user creation; update package dependencies

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,25 +1,30 @@
 import jwt from 'jsonwebtoken'
 
 export default function authMiddleware(req, res, next) {
-  // Get the JWT from the request headers
-  let token = req.headers.authorization?.replace('Bearer ', '');
+
+  //remove this part to active auth
+  req.userId = 1;
+  next();
+
+  // // Get the JWT from the request headers
+  // let token = req.headers.authorization?.replace('Bearer ', '');
  
-  // const accessToken = req.headers.authorization.split(' ')[1];
-  // const decodedToken = jwt.verify(accessToken, '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');
+  // // const accessToken = req.headers.authorization.split(' ')[1];
+  // // const decodedToken = jwt.verify(accessToken, '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');
 
-  if (!token) {
-    return res.status(401).json({ message: 'Authorization header missing or invalid' });
-  }
+  // if (!token) {
+  //   return res.status(401).json({ message: 'Authorization header missing or invalid' });
+  // }
 
-  try {
-    // Verify and decode the JWT
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  // try {
+  //   // Verify and decode the JWT
+  //   const decoded = jwt.verify(token, process.env.JWT_SECRET);
     
-    // check if the userId existed in  table, if not
-    req.userId = decoded.user.userId;
-    next();
-  } catch (err) {
-    return res.status(401).json({ message: 'Invalid or expired token' });
-  }
+  //   // check if the userId existed in  table, if not
+  //   req.userId = decoded.user.userId;
+  //   next();
+  // } catch (err) {
+  //   return res.status(401).json({ message: 'Invalid or expired token' });
+  // }
 }
 
